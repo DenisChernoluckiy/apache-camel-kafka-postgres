d500c39cbd815c44aad2fae97ba003af1d10531e
Denis Chernoluckiy <d.chernoluckiy@tg.dunice.net>
2023-04-12 12:17:03 +0300

    commit with camel-kafka-postgres integration

---
+++ src/test/java/neacron/camelproject/CamelProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package neacron.camelproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
